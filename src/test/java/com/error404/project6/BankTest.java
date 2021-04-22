e30b49599ce8edb6a7ab6c21b4aecab32c14c432
Brianna MacKenzie <briannanmackenzie@gmail.com>
2021-04-22 09:04:38 -0400

    Tests unexpectedly ending at testGetAllAccounts -> assertEquals "FIRST ACCT TEST..." Working on resolution.

--- src/test/java/com/error404/project6/BankTest.java
+++ src/test/java/com/error404/project6/BankTest.java
@@ -1,12 +1,11 @@
 package com.error404.project6;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.SortedSet;
 import java.util.TreeSet;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BankTest {
@@ -16,10 +15,14 @@ class BankTest {
     void setUp() {
         testBank = new Bank("Test Bank");
     }
+    void first(TestInfo testInfo) {
+        String testingThis = testInfo.getTestMethod().get().getName();
+        System.out.println(testingThis);
+    }
 
     /* BEGIN Set Insufficient Funds Penalty Test Suite */
     @Test
-    @DisplayName("Set Insufficient Funds Penalty Test - Negative Case")
+               @DisplayName("Set Insufficient Funds Penalty Test - Negative Case")
     void testSetInsufficientFundsPenalty_NegativeCase() {
         double newIFP = -20.00; // Negative value passed to method
         double insufficientFundsPenalty = 10.00;  // Original value in Bank.java
@@ -54,6 +57,7 @@ class BankTest {
 
     /* BEGIN Get All Accounts Test Suite */
     @Test
+    @DisplayName("Get All Accounts Test - Method Execution")
     void testGetAllAccounts() {
         /* Mock customer data */
         SortedSet<Customer> customers = new TreeSet<>();
@@ -68,20 +72,23 @@ class BankTest {
         for (Customer c : customers) {
             allAccts.add(getCustAcct(c));
         }
-        assertEquals("1", getCustAcct(c1).getAccountId(), "Account ID does not match expected.");
-        assertEquals("2", getCustAcct(c2).getAccountId(), "Account ID does not match expected.");
-    }
-    private Account getCustAcct(Customer c) {
-        Account c1Savings = new SavingsAccount(c, 150.00, "John's Account");
-        Account c2Savings = new SavingsAccount(c, 400.00, "Jame's Account");
-        if (c.getLastName() == "Doe") return c1Savings;
-        else return c2Savings;
+        assertEquals(0, allAccts.first().compareTo(getCustAcct(c1)), "FIRST ACCT TEST: Account ID does not match " +
+                "expected.");
+        /* UNEXPECTED END OF TESTS */
+        assertEquals(1, allAccts.last().compareTo(getCustAcct(c2)), "SECOND ACCT TEST: Account ID does not match " +
+                "expected.");
     }
     /* END Get All Accounts Test Suite */
 
+    /* BEGIN Add Customer Test Suite */
     @Test
+    @DisplayName("Add Customer Test - Method Execution")
     void testAddCustomer() {
+        testBank.addCustomer("Doe", "John");
+        assertEquals(testBank.getCustomer("Doe", "John"), testBank.getCustomer("Does", "John"), "Customer not found. " +
+                "Add operation failed.");
     }
+    /* END Add Customer Test Suite */
 
     @Test
     void testRemoveCustomer() {
@@ -101,5 +108,16 @@ class BankTest {
 
     @Test
     void getCustomersAccounts() {
+    }
+    @AfterEach
+    void last(TestInfo testInfo) {
+        String testingThis = testInfo.getTestMethod().get().getName();
+        System.out.println(testingThis);
+    }
+    private Account getCustAcct(Customer c) {
+        Account c1Savings = new SavingsAccount(c, 150.00, "John's Account");
+        Account c2Savings = new SavingsAccount(c, 400.00, "Jame's Account");
+        if (c.getLastName() == "Doe") return c1Savings;
+        else return c2Savings;
     }
 }
